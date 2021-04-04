0f218d2d979cd420089e2bea0ef003eced3524b1
momonga11 <utarokawa@gmail.com>
2021-04-04 17:22:25 +0900

    fix: VM環境にて、Dockerコンテナを再起動しないとソースコードの変更が反映されない問題の解決

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # システムエラー発生時のリダイレクト先のURL
   config.redirect_system_error_url = "#{ENV.fetch('FRONT_SERVER_ORIGIN', 'http://localhost')}/systemerror"
